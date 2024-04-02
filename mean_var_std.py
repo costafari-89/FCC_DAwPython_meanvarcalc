4c3d924c0471014fbaf60055b3138da3cf6fce35
Mark Costa <markcosta15@gmail.com>
2024-04-02 03:37:49 +0000

    First commit, set up try except

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def calculate(list):
+    input_array = np.array(list)
+    input_3x3 = input_array.reshape(3,3)
 
 
 
---
+++ playground.py
@@ -0,0 +1,4 @@
+import numpy as np
+list = [0,1,2,3,4,5,6,7,8]
+test_array = np.array(list)
+test_3x3 = test_array.reshape(3,3)
