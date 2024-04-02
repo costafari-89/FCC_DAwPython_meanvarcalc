c6f99fe689c27dfb7b8105642a68c02a8beda69b
Mark Costa <markcosta15@gmail.com>
2024-04-02 03:41:54 +0000

    first commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,23 @@ import numpy as np
 
 def calculate(list):
     input_array = np.array(list)
-    input_3x3 = input_array.reshape(3,3)
+
+    try:
+        input_3x3 = input_array.reshape(3,3)
+    
+    except ValueError:
+        print("List must contain nine numbers.")
+
+    else:
+        calculations = {
+            'mean': [axis1, axis2, flattened],
+            'variance': [axis1, axis2, flattened],
+            'standard deviation': [axis1, axis2, flattened],
+            'max': [axis1, axis2, flattened],
+            'min': [axis1, axis2, flattened],
+            'sum': [axis1, axis2, flattened]
+            }
+
 
 
 
